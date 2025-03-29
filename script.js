d99ff6b4f46cc4c50e4f2ecc5cda5a09587a19ae
yarxxxik <91087974+yarxxxik@users.noreply.github.com>
2025-03-29 13:42:31 +0100

    script.js
    Update

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function myFunction () {
     alert("E-mail succesfull delivered!!")
  }
+const scrollContainer = document.querySelector(".reviews-container");
+const scrollSpeed = 1; 
+function infiniteScroll() {
+    if (scrollContainer.scrollLeft >= scrollContainer.scrollWidth - scrollContainer.clientWidth) {
+     
+        scrollContainer.scrollLeft = 0;
+    } else {
+        scrollContainer.scrollLeft += scrollSpeed;
+    }
+}
+
+setInterval(infiniteScroll, 20);
