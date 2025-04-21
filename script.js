46d7567a4209e8f7e434e83ca6145832134b76fd
yarxxxik <91087974+yarxxxik@users.noreply.github.com>
2025-04-21 20:50:40 +0200

    Add files via upload
    my 2-th block

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-function myFunction () {
-    alert("E-mail succesfull delivered!!")
- }
+const myCoords = [59.934280, 30.335099];
+
+const map = L.map('map').setView(myCoords, 16);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map);
+
+L.marker(myCoords).addTo(map)
+  .bindPopup('Nevsky Prospect 28')
+  .openPopup();
